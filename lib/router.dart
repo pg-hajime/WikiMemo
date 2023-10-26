6c880de783f2dcd85e56ed3d65ca2c93f066bbc5
JIMEZ <macuprograming@gmail.com>
2023-10-27 04:34:12 +0900

    編集ページの画面遷移を追加

--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 //TODO ページのimportは１つにまとめたいかも
 import 'package:wikimemo/views/list.dart';
 import 'package:wikimemo/views/memo.dart';
+import 'package:wikimemo/views/edit.dart';
 import 'package:wikimemo/main.dart';
 
 //画面遷移用
@@ -24,6 +25,11 @@ final GoRouter router = GoRouter(
           path: 'details',
           builder: (BuildContext context, GoRouterState state) {
             return MemoPage(state.extra! as String);
+        }),
+        GoRoute(
+          path: 'edit',
+          builder: (BuildContext context, GoRouterState state) {
+            return EditPage();
         }),
       ],
     ),
