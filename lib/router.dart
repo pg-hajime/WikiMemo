61149cd858e574c8c55c1e853c28df924830048b
JIMEZ <macuprograming@gmail.com>
2023-10-27 04:10:10 +0900

    router.dartの作成
    画面遷移用のファイル

---
+++ lib/router.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+//TODO ページのimportは１つにまとめたいかも
+import 'package:wikimemo/views/list.dart';
+import 'package:wikimemo/views/memo.dart';
+import 'package:wikimemo/main.dart';
+
+//画面遷移用
+final GoRouter router = GoRouter(
+
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const MyHomePage(title: 'Home');
+      },
+      routes:  <RouteBase>[
+        GoRoute(
+          path: 'lists',
+          builder: (BuildContext context, GoRouterState state) {
+            return const ListPage();
+        }),
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            return MemoPage(state.extra! as String);
+        }),
+      ],
+    ),
+  ]);
