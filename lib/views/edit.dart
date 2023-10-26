eea1675912a1e2c52a417c3aa62da2d233a468de
JIMEZ <macuprograming@gmail.com>
2023-10-27 05:32:54 +0900

    タイトルを入力できるように変更

--- lib/views/edit.dart
+++ lib/views/edit.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:wikimemo/router.dart';
 
-class EditPage extends StatelessWidget {
+class EditPage extends StatefulWidget {
   const EditPage({super.key});
+
+  @override
+  State<EditPage> createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
+
+  String _text='';
+
+  void _handleText(String e) {
+    setState(() {
+      _text = e;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('編集画面')),
+        title: TextField(
+          decoration: const InputDecoration(
+            labelText: 'タイトルを入力してください'
+          ),
+          onChanged: _handleText,
+        )),
       body: Text('本文'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => router.go('/details', extra: _text),
+        child: const Icon(Icons.check),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
