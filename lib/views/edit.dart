ed454ce26586aed49ed566935160eb3b95a2207f
JIMEZ <macuprograming@gmail.com>
2023-10-27 04:35:52 +0900

    編集ページの修正
    変数titleの削除

--- lib/views/edit.dart
+++ lib/views/edit.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class EditPage extends StatelessWidget {
-  const EditPage(this.title, {super.key});
-
-  final String title;
+  const EditPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title)),
+        title: Text('編集画面')),
       body: Text('本文'),
     );
   }
